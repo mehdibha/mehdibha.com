0cb6655195ae8f980003a09490577d35dd2778eb
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-17 15:21:52 +0100

    update script

--- scripts/update_template.sh
+++ scripts/update_template.sh
@@ -4,9 +4,9 @@
 git fetch template
 
 # Merge changes from the template repository while excluding specific files
-git merge template/main --no-commit
+git merge -s recursive -X theirs template/main --allow-unrelated-histories --no-commit
 git reset ./public ./src/config/site.config.ts
-git commit -m "Merge changes from template (excluding certain files)"
+# git commit -m "Update template"
 
 # Push changes to your website repository
 # git push origin main
