37d2aedb49e5a6376d4da22a4bad9d2b43314686
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-21 16:36:20 +0100

    add contact

--- src/config/site.ts
+++ src/config/site.ts
@@ -194,4 +194,12 @@ export const siteConfig: ISiteConfig = {
       },
     },
   ],
+  contact: {
+    title: "Get in touch",
+    description: "Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!",
+    callToAction: {
+      label : "Say hello",
+      href: "emailto:mehdibenhadjali@gmail.com"
+    }
+  }
 }
